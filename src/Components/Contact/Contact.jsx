9e0a23938b768572b33168e62863a15aff2da8ff
Loriane MICHEL <loriane.michel94@gmail.com>
2023-11-26 19:23:35 +0100

    add : netlify form

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,36 +1,47 @@
+import React from 'react';
 import { useTranslation } from "react-i18next";
-
+import NetlifyForm from 'react-netlify-form';
 
 export default function Contact() {
+  const { t } = useTranslation();
 
-    const { t } = useTranslation();
-
-    return (
-        <div className="contact-container">
-            <form name="contact" method="POST" data-netlify="true" className="contact-container-form">
+  return (
+    <div className="contact-container">
+      <NetlifyForm name='contact'>
+        {({ loading, error, success }) => (
+          <div>
+            {loading &&
+              <div>{t('contact.loading')}</div>
+            }
+            {error &&
+              <div>{t('contact.error')}</div>
+            }
+            {success &&
+              <div>{t('contact.success')}</div>
+            }
+            {!loading && !success &&
+              <form className="contact-container-form">
                 <input type="hidden" name="form-name" value="contact" />
-
                 <div className="input-group">
-                    <label htmlFor="name">{t('contact.form.name')}</label>
-                    <input type="text" id="name" name="name" required />
+                  <label htmlFor="name">{t('contact.form.name')}</label>
+                  <input type="text" id="name" name="name" required />
                 </div>
-
                 <div className="input-group">
-                    <label htmlFor="email">{t('contact.form.email')}</label>
-                    <input type="email" id="email" name="email" required />
+                  <label htmlFor="email">{t('contact.form.email')}</label>
+                  <input type="email" id="email" name="email" required />
                 </div>
-
                 <div className="input-group">
-                    <label htmlFor="message">{t('contact.form.message')}</label>
-                    <textarea id="message" name="message" rows="4" required></textarea>
+                  <label htmlFor="message">{t('contact.form.message')}</label>
+                  <textarea id="message" name="message" rows="4" required></textarea>
                 </div>
-
                 <div className="input-group button__div">
-                    <button type="submit">{t('contact.button')}</button>
+                  <button type="submit">{t('contact.button')}</button>
                 </div>
-            </form>
-        </div>
-    );
+              </form>
+            }
+          </div>
+        )}
+      </NetlifyForm>
+    </div>
+  );
 }
-
-
