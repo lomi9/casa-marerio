bf7dd28111ff00f5d0993977c123e29926d30854
Loriane MICHEL <loriane.michel94@gmail.com>
2023-11-26 21:54:44 +0100

    .

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -2,7 +2,6 @@ import React, { useState, useRef } from 'react';
 import { useTranslation } from "react-i18next";
 import emailjs from 'emailjs-com';
 
-
 export default function Contact() {
     const { t } = useTranslation();
     const [formSent, setFormSent] = useState(false);
@@ -12,17 +11,28 @@ export default function Contact() {
     const templateID = process.env.REACT_APP_EMAILJS_TEMPLATE_ID;
     const userID = process.env.REACT_APP_EMAILJS_USER_ID;
 
-
     const sendEmail = (e) => {
-        e.preventDefault(); // Empêche le formulaire d'envoyer une requête POST classique
+        e.preventDefault();
 
-        emailjs.sendForm(serviceID, templateID, formRef.current, userID)
-            .then((result) => {
-                console.log(result.text);
-                setFormSent(true); // Mettre à jour l'état pour indiquer l'envoi réussi
-            }, (error) => {
-                console.log(error.text);
-            });
+        // Récupérer la réponse reCAPTCHA du champ caché généré par Google reCAPTCHA
+        const recaptchaResponse = document.getElementById('g-recaptcha-response').value;
+
+        if (recaptchaResponse) {
+            // Ajouter la réponse reCAPTCHA aux données du formulaire
+            const formData = new FormData(formRef.current);
+            formData.append('g-recaptcha-response', recaptchaResponse);
+
+            emailjs.sendForm(serviceID, templateID, formData, userID)
+                .then((result) => {
+                    console.log(result.text);
+                    setFormSent(true); // Indiquer que le formulaire a été envoyé avec succès
+                }, (error) => {
+                    console.log(error.text);
+                });
+        } else {
+            // Vous pouvez gérer le cas où reCAPTCHA n'est pas résolu comme bon vous semble
+            console.log('Please resolve the reCAPTCHA.');
+        }
     };
 
     return (
@@ -44,14 +54,16 @@ export default function Contact() {
                     <label htmlFor="message">{t('contact.form.message')}</label>
                     <textarea id="message" name="message" rows="4" required></textarea>
                 </div>
-                <div class="g-recaptcha" data-sitekey="6LekdR0pAAAAAAC8yKCVc0HVxI1N-k61VjEmkam7"></div>
 
+                <div className="g-recaptcha" data-sitekey="6LekdR0pAAAAAAC8yKCVc0HVxI1N-k61VjEmkam7"></div>
 
                 <div className="input-group button__div">
                     <button type="submit">{formSent ? t('contact.sent') : t('contact.button')}</button>
                 </div>
             </form>
-            {formSent && <p>{t('contact.thankYouMessage')}</p>}
+            {formSent && <p>{t('contact.success')}</p>}
         </div>
     );
 }
+
+
