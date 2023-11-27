76f4514dfebd9d8b21efbdc7890e1ba7623e619d
Loriane MICHEL <loriane.michel94@gmail.com>
2023-11-27 11:18:10 +0100

    Add reCAPTCHA to contact form for spam prevention

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,34 +1,45 @@
 import React, { useState, useRef } from 'react';
 import { useTranslation } from "react-i18next";
 import emailjs from 'emailjs-com';
+import ReCAPTCHA from "react-google-recaptcha";
 
 export default function Contact() {
     const { t } = useTranslation();
     const [formStatus, setFormStatus] = useState({ sent: false, message: '' });
+    const [captchaValue, setCaptchaValue] = useState(null);
     const formRef = useRef(); 
 
     const serviceID = process.env.REACT_APP_EMAILJS_SERVICE_ID;
     const templateID = process.env.REACT_APP_EMAILJS_TEMPLATE_ID;
     const userID = process.env.REACT_APP_EMAILJS_USER_ID;
 
+    const onChangeCaptcha = (value) => {
+        setCaptchaValue(value);
+    };
+
     const sendEmail = (e) => {
         e.preventDefault();
+
+        if (!captchaValue) {
+            setFormStatus({ sent: false, message: t('contact.captcha_error') }); // Message d'erreur si le CAPTCHA n'est pas résolu
+            return;
+        }
 
         emailjs.sendForm(serviceID, templateID, formRef.current, userID)
         .then((result) => {
             console.log(result.text);
             setFormStatus({ sent: true, message: t('contact.success') }); // Message de succès
             formRef.current.reset(); // Réinitialiser le formulaire en cas de succès
+            setCaptchaValue(null); // Réinitialiser le CAPTCHA
         }, (error) => {
             console.log(error.text);
             setFormStatus({ sent: false, message: t('contact.error') }); // Message d'erreur
         });
-};
+    };
 
     return (
-
         <div className="contact-container">
-             <p className="contact-container-msg">{formStatus.message}</p>
+            <p className="contact-container-msg">{formStatus.message}</p>
             <form ref={formRef} name="template_clst4ep" className="contact-container-form" onSubmit={sendEmail}>
                 <input type="hidden" name="form-name" value="template_clst4ep" />
 
@@ -47,14 +58,15 @@ export default function Contact() {
                     <textarea id="message" name="message" rows="4" required></textarea>
                 </div>
 
+                <ReCAPTCHA
+                    sitekey={process.env.REACT_APP_RECAPTCHA_SITE_KEY}
+                    onChange={onChangeCaptcha}
+                />
 
                 <div className="input-group button__div">
-                    <button type="submit">{formStatus ? t('contact.button') : t('contact.button')}</button>
+                    <button type="submit">{t('contact.button')}</button>
                 </div>
             </form>
         </div>
-
     );
 }
-
-
