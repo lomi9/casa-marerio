908de92f196c3a59cc232f6c1b5ebf034133e28f
lomi9 <119067005+lomi9@users.noreply.github.com>
2024-02-03 15:59:48 +0100

    fix : zoom on banner img on iphone

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -10,10 +10,10 @@ export default function Banner() {
     return (
 
       <div className="banner__div">
-        <h1 className="banner__h1"> {t('banner.title')}</h1>
-        <div className="banner__scroll">
-<a href='#content' className="banner__scroll-p"> {t('banner.scroll')}</a>
-<a href='#content'><FontAwesomeIcon icon={faAngleDown} bounce className="banner__scroll-fleche" /></a>
+        <h1 className="banner__div-h1"> {t('banner.title')}</h1>
+        <div className="banner__div-scroll">
+<a href='#content' className="banner__div-scroll-p"> {t('banner.scroll')}</a>
+<a href='#content'><FontAwesomeIcon icon={faAngleDown} bounce className="banner__div-scroll-fleche" /></a>
         </div>
       </div>
     );
